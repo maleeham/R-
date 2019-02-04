ddb4b2b63f66b3f92ec9d0671caf5f094ce27214
Maleeha S. Koul <msk180001@utdallas.edu>
2019-02-04 14:52:27 -0600

    Create Exploratory data analysis.r

---
+++ Exploratory data analysis.r
@@ -0,0 +1,44 @@
+#Load Datasets
+train <- read.csv("Train_UWu5bXk.csv")
+test <- read.csv("Test_u94Q5KV.csv")
+
+#check dimesions ( number of row & columns) in data set
+> dim(train)
+[1] 8523 12
+
+> dim(test)
+[1] 5681 11
+
+#check the variables and their types in train
+> str(train)
+'data.frame': 8523 obs. of 12 variables:
+$ Item_Identifier : Factor w/ 1559 levels "DRA12","DRA24",..: 157 9 663 1122 1298 759 697 739 441 991 ...
+$ Item_Weight : num 9.3 5.92 17.5 19.2 8.93 ...
+$ Item_Fat_Content : Factor w/ 5 levels "LF","low fat",..: 3 5 3 5 3 5 5 3 5 5 ...
+$ Item_Visibility : num 0.016 0.0193 0.0168 0 0 ...
+$ Item_Type : Factor w/ 16 levels "Baking Goods",..: 5 15 11 7 10 1 14 14 6 6 ...
+$ Item_MRP : num 249.8 48.3 141.6 182.1 53.9 ...
+$ Outlet_Identifier : Factor w/ 10 levels "OUT010","OUT013",..: 10 4 10 1 2 4 2 6 8 3 ...
+$ Outlet_Establishment_Year: int 1999 2009 1999 1998 1987 2009 1987 1985 2002 2007 ...
+$ Outlet_Size : Factor w/ 4 levels "","High","Medium",..: 3 3 3 1 2 3 2 3 1 1 ...
+$ Outlet_Location_Type : Factor w/ 3 levels "Tier 1","Tier 2",..: 1 3 1 3 3 3 3 3 2 2 ...
+$ Outlet_Type : Factor w/ 4 levels "Grocery Store",..: 2 3 2 1 2 3 2 4 2 2 ...
+$ Item_Outlet_Sales : num 3735 443 2097 732 995 ...
+
+> table(is.na(train))
+
+FALSE TRUE 
+100813 1463
+
+> colSums(is.na(train))
+Item_Identifier Item_Weight 
+0                1463 
+Item_Fat_Content Item_Visibility 
+0                 0 
+Item_Type         Item_MRP 
+0                 0 
+Outlet_Identifier Outlet_Establishment_Year 
+0                 0 
+Outlet_Size       Outlet_Location_Type 
+0                 0 
+> summary(train)
